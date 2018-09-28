d2586fe0de79bf6ce1270a68ab4e2345ed7223cc
Kamal <kamal.m@live.com>
2018-09-28 14:23:30 +1200

    Navigation working but only using one activity and change 3 fragments.  Needs to be changed so that another activity is open when shop is pressed

---
+++ app/src/main/java/com/example/kamal/cricketapp/FragmentFirst.java
@@ -0,0 +1,17 @@
+package com.example.kamal.cricketapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentFirst extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_first, container, false);
+    }
+}
--- app/src/main/java/com/example/kamal/cricketapp/MainActivity.java
+++ app/src/main/java/com/example/kamal/cricketapp/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.kamal.cricketapp;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private DrawerLayout drawer;
 
     @Override
@@ -15,19 +19,65 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // use the toolbar we made and use that, make the toolbar the action bar
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
         drawer = findViewById(R.id.drawer_layout);
 
+        NavigationView navgationView = findViewById(R.id.nav_view);
+        navgationView.setNavigationItemSelectedListener(this);
+
+        // this will help rotate the hamburger icon when the navigation drawer is open
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        // the first fragment will be shown when the app has started
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new FragmentFirst()).commit();
+            navgationView.setCheckedItem(R.id.nav_home);
+        }
     }
 
+    // when one of the items has been clicked change the fragment
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new FragmentFirst()).commit();
+                break;
+            case R.id.nav_shop:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new FragmentSecond()).commit();
+                break;
+            case R.id.nav_cart:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new FragmentThird()).commit();
+                break;
+            case R.id.nav_contact:
+                Toast.makeText(this, "Contact", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_settings:
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    //the nav drawer will close when the back button pressed
     @Override
     public void onBackPressed() {
+
+        //the nav drawer will close when the back button pressed
+
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
